2d85a7f34c4b3dc97416b7ed8f40bcab8e766a23
Brandon Haynes <brandon@Brandons-MacBook-Pro.local>
2018-08-22 23:21:37 -0700

    troubleshooting with Kat and minor corrections from class video

---
+++ stocks_api/views/stocks.py
@@ -0,0 +1,24 @@
+from pyramid_restful.viewsets import APIViewSet
+from pyramid.response import Response
+
+
+class StocksAPIView(APIViewSet):
+    # GET all
+    def list(self, request):
+        pass
+        return Response(json={'message': 'Listing all the records.'}, status=200)
+
+    # GET one
+    def retrieve(self, request):
+        pass
+        return Response(json={'message': 'Listing one of the records.'}, status=200)
+
+    # POST
+    def create(self, request):
+        pass
+        return Response(json={'message': 'Created a new record.'}, status=201)
+
+    # DELETE
+    def destroy(self, request):
+        pass
+        return Response(json={'message': 'Deleted the record.'}, status=204)
